067dd22ace2731fd1d0c787584fb0cb20ca3e9b5
Dipl.-Ing. Varkoly Peter <pvarkoly@cephalix.eu>
2020-06-13 13:29:09 +0200

    Separate CRANIX and CEPHALIX migration script.

---
+++ dao/data/updates/015-migrate-to-cranix.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ -e /var/adm/cranix/migrated-to-cranix ]; then
+	exit 0
+fi
+export HOME="/root"
+CRANIX=$( echo "show tables" | mysql CRX | grep CrxNextID )
+if [ "${CRANIX}" ];  then
+	exit 0
+fi
+
+DATE=$( date +%Y-%m-%d-%H-%M )
+echo "INSERT INTO Enumerates VALUES(NULL,'apiAcl','hwconf.modify',6);" | mysql OSS
+mysqldump --databases OSS | gzip > OSS-BACKUP-${DATE}.sql.gz
+
+echo "RENAME TABLE OssNextID TO CrxNextID;"     | mysql OSS
+echo "RENAME TABLE OSSConfig  TO CrxConfig"     | mysql OSS
+echo "RENAME TABLE OSSMConfig TO CrxMConfig"    | mysql OSS
+echo "RENAME TABLE OssResponses TO CrxResponse" | mysql OSS
+mysqldump --databases OSS > CRX.sql
+sed -i '1,26s/OSS/CRX/' CRX.sql
+mysql < CRX.sql
+password=$( mktemp cranixXXXXXXXXXXXX )
+sed -i s/javax.persistence.jdbc.password=.*$/javax.persistence.jdbc.password=${password}/ /opt/cranix-java/conf/cranix-api.properties
+sed -i 's/=claxss/=cranix/' /opt/cranix-java/conf/cranix-api.properties
+echo "grant all on CRX.* to 'cranix'@'localhost'  identified by '$password'" | mysql
+
+touch /var/adm/cranix/migrated-to-cranix
