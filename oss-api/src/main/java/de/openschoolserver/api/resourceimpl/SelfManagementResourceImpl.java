b0a1912e9e5ff789b047c892394a730ae2ab3608
Dipl.-Ing. Varkoly Peter <pvarkoly@cephalix.eu>
2020-09-09 10:32:19 +0200

    User plugin when everyting was changend. Do not call samba-tool directly

--- oss-api/src/main/java/de/openschoolserver/api/resourceimpl/SelfManagementResourceImpl.java
+++ oss-api/src/main/java/de/openschoolserver/api/resourceimpl/SelfManagementResourceImpl.java
@@ -48,53 +48,32 @@ public class SelfManagementResourceImpl implements SelfManagementResource {
 		User oldUser = session.getUser();
 		OssResponse  ossResponse = null;
 		logger.debug("modifyMySelf" + user);
-		if( userController.isAllowed("myself.manage") ) {
-			if( user.getPassword() != null && !user.getPassword().isEmpty() ) {
-				ossResponse = userController.checkPassword(user.getPassword());
-				logger.debug("Check-Password:" + ossResponse );
-				if( ossResponse != null  && ossResponse.getCode().equals("ERROR")) {
-					return ossResponse;
-				}
-				oldUser.setPassword(user.getPassword());
+		if( user.getPassword() != null && !user.getPassword().isEmpty() ) {
+			ossResponse = userController.checkPassword(user.getPassword());
+			logger.debug("Check-Password:" + ossResponse );
+			if( ossResponse != null  && ossResponse.getCode().equals("ERROR")) {
+				return ossResponse;
 			}
+			oldUser.setPassword(user.getPassword());
+		}
+		if( userController.isAllowed("myself.manage") ) {
 			oldUser.setGivenName(user.getGivenName());
 			oldUser.setSurName(user.getSurName());
 			oldUser.setBirthDay(user.getBirthDay());
 			oldUser.setFsQuota(user.getFsQuota());
 			oldUser.setMsQuota(user.getMsQuota());
-			try {
-				em.getTransaction().begin();
-				em.merge(oldUser);
-				em.getTransaction().commit();
-				startPlugin("modify_user", oldUser);
-			} catch (Exception e) {
-				return null;
-			} finally {
-				em.close();
-			}
-			ossResponse = new OssResponse(session,"OK","User parameters were set successfully.");
-		} else {
-			if( user.getPassword() != null && !user.getPassword().isEmpty() ) {
-				ossResponse = userController.checkPassword(user.getPassword());
-				em.close();
-				if( ossResponse != null  && ossResponse.getCode().equals("ERROR")) {
-					logger.debug("checkPassword:" + ossResponse);
-					return ossResponse;
-				}
-				StringBuffer reply = new StringBuffer();
-				StringBuffer error = new StringBuffer();
-				String[]   program = new String[5];
-				program[0] = "/usr/bin/samba-tool";
-				program[1] = "user";
-				program[2] = "setpassword";
-				program[3] = session.getUser().getUid();
-				program[4] = "--newpassword=" + user.getPassword();
-				OSSShellTools.exec(program, reply, error, null);
-				logger.debug("sambatool:" + reply.toString() + " Error" + error.toString() );
-				ossResponse = new OssResponse(session,"OK","User parameters were set successfully.");
-			}
 		}
-		return ossResponse;
+		try {
+			em.getTransaction().begin();
+			em.merge(oldUser);
+			em.getTransaction().commit();
+			startPlugin("modify_user", oldUser);
+		} catch (Exception e) {
+			return OssResponse(session,"ERROR","Could not modify user parameter.");
+		} finally {
+			em.close();
+		}
+		return new OssResponse(session,"OK","User parameters were set successfully.");
 	}
 
 	@Override
