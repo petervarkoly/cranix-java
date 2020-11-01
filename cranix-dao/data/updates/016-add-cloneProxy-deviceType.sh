eff04c1d9cf9858dafc2f54a86f72ed44e0b018c
Dipl.-Ing. Varkoly Peter <pvarkoly@cephalix.eu>
2020-11-01 13:21:50 +0100

    Create a new HWConf with type clone proxy.

--- cranix-dao/data/updates/016-add-cloneProxy-deviceType.sh
+++ cranix-dao/data/updates/016-add-cloneProxy-deviceType.sh
@@ -3,4 +3,5 @@
 CLONEPROXY=$( echo "select * from Enumerates where name='deviceType' and value='cloneProxy'" | mysql CRX )
 if [ -z "$CLONEPROXY" ]; then
 	echo "INSERT INTO Enumerates VALUES(NULL,'deviceType','cloneProxy',1);" | mysql CRX
+	echo "INSERT INTO HWConfs VALUES(NULL,'CloneProxy','Clone Proxy for WLAN Devices','cloneProxy',1);" | mysql CRX
 fi
