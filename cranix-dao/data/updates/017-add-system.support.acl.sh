b899542cf9287a7865b6866c5542c0bfd1fd7df5
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-11-28 09:47:16 +0100

    Actualize and regorganize the package

--- cranix-dao/data/updates/017-add-system.support.acl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SUPPORTACL=$( echo "select * from Enumerates where name='apiAcl' and value='system.support'" | mysql CRX )
-if [ -z "$SUPPORTACL" ]; then
-	echo "INSERT INTO Enumerates VALUES(NULL,'apiAcl','system.support',1);" | mysql CRX
-	echo "INSERT INTO Acls VALUES(NULL,NULL,1,'system.support','Y',1);" | mysql CRX
-	touch /run/cranix-db-changed
-fi
