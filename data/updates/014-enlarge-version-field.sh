61fd29fb6e9927dfd91e636bcb1c9ec49cae9cf0
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2021-05-12 20:09:57 +0200

    Adapt changes from tob-ses

--- data/updates/014-enlarge-version-field.sh
+++ data/updates/014-enlarge-version-field.sh
@@ -2,9 +2,9 @@
 export HOME="/root"
 
 
-SIZE=$( echo "describe SoftwareVersions" | mysql OSS | grep version | gawk '{ print $2 }' )
+SIZE=$( echo "describe SoftwareVersions" | mysql CRX | grep version | gawk '{ print $2 }' )
 if [ $SIZE != 'varchar(128)' ]
 then
-	echo "ALTER TABLE SoftwareVersions MODIFY COLUMN version VARCHAR(128)" | mysql OSS
+	echo "ALTER TABLE SoftwareVersions MODIFY COLUMN version VARCHAR(128)" | mysql CRX
 	touch /run/cranix-db-changed
 fi
