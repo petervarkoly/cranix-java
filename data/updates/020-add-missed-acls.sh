3d1ead5b5730d03d1aa0317758dad59a75630969
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2021-10-10 17:22:26 +0200

    Now we propagate the right system.superuser. This is needed to be able to read all files on the file system. But this right will not be assigned to any group or user. printers.add role can be set for all sysadmin user.

--- data/updates/020-add-missed-acls.sh
+++ data/updates/020-add-missed-acls.sh
@@ -2,3 +2,4 @@
 
 /usr/sbin/crx_api.sh PUT  system/enumerates/apiAcl/hwconf.delete
 /usr/sbin/crx_api.sh POST system/acls/groups/1 '{"acl":"hwconf.delete","allowed":true,"userId":null,"groupId":1}'
+/usr/sbin/crx_api.sh PUT  system/enumerates/apiAcl/system.superuser
