f4631bd0698af878c73208ae2ad1ed0da509074d
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2023-02-07 15:04:14 +0100

    Add update script to create some teaching subjects.

---
+++ data/updates/022-add-subjects.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SUBJECTS="Chemie
+Deutsch
+English
+Ethik
+Französisch
+Geschichte
+Geographie
+Informatik
+Latein
+Literatur
+Mathematik
+Musik
+Philosophie
+Physik
+Religion
+Sport
+Technik"
+for i in ${SUBJECTS}
+do
+     	echo "{\"id\":null,\"name\":\"${i}\"}" > /tmp/add_subject.json
+	/usr/sbin/crx_api_post_file.sh objects/subjects  /tmp/add_subject.json
+done
