8376130c3230414970810f008d4309051df4cf26
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2025-03-26 21:07:59 +0100

    Remove not used files

--- data/updates/001-add-missed-acls.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/usr/sbin/crx_api.sh PUT  system/enumerates/apiAcl/parent.manage
-/usr/sbin/crx_api.sh POST system/acls/groups/1 '{"acl":"parent.manage","allowed":true,"userId":null,"groupId":1}'
-/usr/sbin/crx_api.sh PUT  system/enumerates/apiAcl/ptm.manage
-/usr/sbin/crx_api.sh POST system/acls/groups/1 '{"acl":"ptm.manage","allowed":true,"userId":null,"groupId":1}'
-/usr/sbin/crx_api.sh PUT  system/enumerates/apiAcl/ptm.use
-/usr/sbin/crx_api.sh POST system/acls/groups/1 '{"acl":"ptm.use","allowed":true,"userId":null,"groupId":1}'
-
