38e0f8beface5e715070d23d658c3f75794855d3
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2025-11-17 16:58:13 +0100

    Fix tipo

--- data/updates/010-adapt-database.sh
+++ data/updates/010-adapt-database.sh
@@ -6,4 +6,5 @@ echo "ALTER TABLE Sessions MODIFY validUntil timestamp;" | mysql CRX
 echo "ALTER TABLE Sessions MODIFY validFrom timestamp;" | mysql CRX
 echo "ALTER TABLE Crx2faSessions DROP FOREIGN KEY  FK_Sessions_session_id;" | mysql CRX
 echo "ALTER TABLE Crx2faSessions DROP session_id;" | mysql CRX
+echo "ALTER TABLE Users RENAME COLUMN telefonNumber TO telephoneNumber;" | mysql CRX
 
