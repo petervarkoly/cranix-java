affced9c05a3cdb00722ec30ebfa2abbcb2d2557
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2026-03-27 17:26:19 +0100

    New functionality to manage tickets without mail system.

--- src/main/java/de/cranix/services/SupportService.java
+++ src/main/java/de/cranix/services/SupportService.java
@@ -19,135 +19,159 @@ import de.cranix.dao.CrxResponse;
 import de.cranix.dao.Session;
 import de.cranix.dao.SupportRequest;
 import de.cranix.helper.CrxSystemCmd;
+
 import static de.cranix.helper.CranixConstants.*;
 
 public class SupportService extends Service {
-	Logger logger = LoggerFactory.getLogger(SupportService.class);
+    Logger logger = LoggerFactory.getLogger(SupportService.class);
 
-	private String supportUrl;
-        private String supportEmail;
-        private String supportEmailFrom;
+    private String supportUrl;
+    private String supportEmailFrom;
+    private String regCode;
 
-	public SupportService(Session session, EntityManager em) {
-		super(session, em);
-	}
+    public SupportService(Session session, EntityManager em) {
+        super(session, em);
+        supportUrl = this.getConfigValue("SUPPORT_URL");
+        regCode = this.getConfigValue("REG_CODE");
+        if (supportUrl == null) {
+            supportUrl = cranixSupportUrl;
+        }
+        supportUrl = supportUrl.trim();
+    }
 
-	public CrxResponse create(SupportRequest supportRequest) {
-		supportUrl = this.getConfigValue("SUPPORT_URL");
-                if(supportUrl != null) {
-                        supportUrl = supportUrl.trim();
-                }
-		if(supportUrl != null && supportUrl.equalsIgnoreCase("MAIL")) {
-                        supportEmail     = this.getConfigValue("SUPPORT_MAIL_ADDRESS");
-                        supportEmailFrom = supportRequest.getEmail(); 
-                        if(supportEmailFrom.isEmpty()) {
-				this.getConfigValue("SUPPORT_MAIL_FROM");
-                                supportEmailFrom = supportEmailFrom.trim();
-                                if (supportEmailFrom.length()==0) {
-                                        supportEmailFrom = null;
-                                }
-                        }
-                        supportUrl = null;
-                } else if (supportUrl != null && supportUrl.length() > 0) {
-                        supportEmail = null;
-                        supportEmailFrom = null;
-                } else {
-                        supportUrl = cranixSupportUrl;
-                        supportEmail = null;
-                        supportEmailFrom = null;
-                }
-		if( supportRequest.getRegcode() == null || supportRequest.getRegcode().isEmpty() )  {
-			supportRequest.setRegcode(this.getConfigValue("REG_CODE"));
-		}
-		if( supportRequest.getProduct() == null || supportRequest.getProduct().isEmpty() )  {
-			supportRequest.setProduct("CRANIX");
-		}
-		if( supportRequest.getCompany() == null || supportRequest.getCompany().isEmpty() )  {
-			supportRequest.setCompany(this.getConfigValue("NAME"));
-		}
-		List<String> parameters  = new ArrayList<String>();
-		logger.debug("URL: " + supportUrl);
-		logger.debug(supportRequest.toString());
-		File file = null;
-		try {
-			file = File.createTempFile("support", ".json", new File(cranixTmpDir));
-			PrintWriter writer = new PrintWriter(file.getPath(), "UTF-8");
-			writer.print(supportRequest.toString());
-			writer.close();
-		} catch (IOException e) {
-			logger.error(e.getMessage(), e);
-			return new CrxResponse("ERROR", e.getMessage());
-		}
-		if(supportUrl != null && supportUrl.length() > 0) {
-			String[] program    = new String[12];
-			StringBuffer reply  = new StringBuffer();
-			StringBuffer stderr = new StringBuffer();
-			program[0] = "/usr/bin/curl";
-			program[1] = "--insecure";
-			program[2] = "-s";
-			program[3] = "-X";
-			program[4] = "POST";
-			program[5] = "--header";
-			program[6] = "Content-Type: application/json";
-			program[7] = "--header";
-			program[8] = "Accept: application/json";
-			program[9] = "-d";
-			program[10] = "@"+file.getPath();
-			program[11] = supportUrl;
-			CrxSystemCmd.exec(program, reply, stderr, null);
-			logger.debug("Support reply" + reply.toString());
-			logger.debug("Support error" + stderr.toString());
-			try {
-				ObjectMapper mapper = new ObjectMapper();
-				SupportRequest suppres = mapper.readValue(IOUtils.toInputStream(reply.toString(), "UTF-8"), SupportRequest.class);
-				logger.debug("Support Respons :" + suppres);
-				if( suppres.getStatus() != null && suppres.getStatus().equals("OK") ) {
-					parameters.add(suppres.getSubject());
-					parameters.add(suppres.getTicketno());
-					parameters.add(suppres.getEmail());
-					parameters.add(suppres.getTicketResponseInfo());
-					return new CrxResponse("OK","Support request '%s' was created with ticket number '%s'. Answer will be sent to '%s'.",null,parameters);
-				} else {
-					parameters.add(suppres.getSubject());
-					parameters.add(suppres.getTicketResponseInfo());
-					return new CrxResponse("ERROR","Support request '%s' was not created. The reason is:'%s'",null,parameters);
-				}
-			} catch (Exception e) {
-				logger.error("GETObject :" + e.getMessage());
-				return new CrxResponse("ERROR","Can not send support request");
-			}
-		} else {
-			// Support via email
-			StringBuilder request = new StringBuilder();
-			//Header
-			//Subject:
-			request.append("Subject: ").append(supportRequest.getSubject()).append("\n");
-			//From:
-			request.append("From: ").append(supportRequest.getFirstname()).append(" ").append(supportRequest.getLastname()).append(" <").append(supportEmailFrom).append(">");
-			request.append("\n");
-			//Start body
-			request.append(supportRequest.getDescription());
-			request.append("\n\nRegcode: ").append(supportRequest.getRegcode());
-			request.append("\nSupporttype: ").append(supportRequest.getSupporttype());
-			request.append("\n.");
+    public CrxResponse create(SupportRequest supportRequest) {
+        supportEmailFrom = supportRequest.getEmail();
+        if (supportRequest.getRegcode() == null || supportRequest.getRegcode().isEmpty()) {
+            supportRequest.setRegcode(regCode);
+        }
+        if (supportRequest.getProduct() == null || supportRequest.getProduct().isEmpty()) {
+            supportRequest.setProduct("CRANIX");
+        }
+        if (supportRequest.getCompany() == null || supportRequest.getCompany().isEmpty()) {
+            supportRequest.setCompany(this.getConfigValue("NAME"));
+        }
+        List<String> parameters = new ArrayList<String>();
+        logger.debug("URL: " + supportUrl);
+        logger.debug(supportRequest.toString());
+        File file = null;
+        try {
+            file = File.createTempFile("support", ".json", new File(cranixTmpDir));
+            PrintWriter writer = new PrintWriter(file.getPath(), "UTF-8");
+            writer.print(supportRequest.toString());
+            writer.close();
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            return new CrxResponse("ERROR", e.getMessage());
+        }
 
-			String[] program   = new String[2];
-			StringBuffer reply = new StringBuffer();
-			StringBuffer error = new StringBuffer();
-			program[0] = "/usr/sbin/sendmail";
-			program[1] = supportEmail;
+        String[] program = new String[12];
+        StringBuffer reply = new StringBuffer();
+        StringBuffer stderr = new StringBuffer();
+        program[0] = "/usr/bin/curl";
+        program[1] = "--insecure";
+        program[2] = "-s";
+        program[3] = "-X";
+        program[4] = "POST";
+        program[5] = "--header";
+        program[6] = "Content-Type: application/json";
+        program[7] = "--header";
+        program[8] = "Accept: application/json";
+        program[9] = "-d";
+        program[10] = "@" + file.getPath();
+        program[11] = supportUrl;
+        CrxSystemCmd.exec(program, reply, stderr, null);
+        logger.debug("Support reply" + reply.toString());
+        logger.debug("Support error" + stderr.toString());
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            SupportRequest suppres = mapper.readValue(IOUtils.toInputStream(reply.toString(), "UTF-8"), SupportRequest.class);
+            logger.debug("Support Respons :" + suppres);
+            if (suppres.getStatus() != null && suppres.getStatus().equals("OK")) {
+                parameters.add(suppres.getSubject());
+                parameters.add(suppres.getTicketno());
+                parameters.add(suppres.getEmail());
+                parameters.add(suppres.getTicketResponseInfo());
+                return new CrxResponse("OK", "Support request '%s' was created with ticket number '%s'. Answer will be sent to '%s'.", null, parameters);
+            } else {
+                parameters.add(suppres.getSubject());
+                parameters.add(suppres.getTicketResponseInfo());
+                return new CrxResponse("ERROR", "Support request '%s' was not created. The reason is:'%s'", null, parameters);
+            }
+        } catch (Exception e) {
+            logger.error("GETObject :" + e.getMessage());
+            return new CrxResponse("ERROR", "Can not send support request");
+        }
+    }
 
-			int result = CrxSystemCmd.exec(program, reply, error, request.toString());
-			if (result == 0) {
-				parameters.add(supportRequest.getSubject());
-				parameters.add(supportRequest.getEmail());
-					return new CrxResponse("OK","Support request '%s' was sent.  Answer will be sent to '%s'.",null,parameters);
-			} else {
-				logger.error("Error sending support mail: " + error.toString());
-				parameters.add(supportRequest.getSubject());
-				parameters.add(error.toString());
-				return new CrxResponse("ERROR","Sopport request '%s' could not be sent. Reason '%s'",null, parameters);
-			}
-		}
-	}
+    public Object getAll(String status) {
+        String url = "http://zadmin:9080/api/tickets/regcodes/" + regCode + "/cranixUserId/" + this.session.getUser().getId() + "/" + status;
+        if(isSuperuser()){
+            url = "http://zadmin:9080/api/tickets/regcodes/" + regCode + "/status/" + status;
+        }
+        String[] program = new String[10];
+        StringBuffer reply = new StringBuffer();
+        StringBuffer stderr = new StringBuffer();
+        program[0] = "/usr/bin/curl";
+        program[1] = "--insecure";
+        program[2] = "-s";
+        program[3] = "-X";
+        program[4] = "GET";
+        program[5] = "--header";
+        program[6] = "Content-Type: application/json";
+        program[7] = "--header";
+        program[8] = "Accept: application/json";
+        program[9] = url;
+        CrxSystemCmd.exec(program, reply, stderr, null);
+        return reply.toString();
+    }
+
+    public Object getArticle(Long ticketId) {
+        String url = "http://zadmin:9080/api/tickets/regcodes/" + regCode + "/" + ticketId;
+        String[] program = new String[10];
+        StringBuffer reply = new StringBuffer();
+        StringBuffer stderr = new StringBuffer();
+        program[0] = "/usr/bin/curl";
+        program[1] = "--insecure";
+        program[2] = "-s";
+        program[3] = "-X";
+        program[4] = "GET";
+        program[5] = "--header";
+        program[6] = "Content-Type: application/json";
+        program[7] = "--header";
+        program[8] = "Accept: application/json";
+        program[9] = url;
+        CrxSystemCmd.exec(program, reply, stderr, null);
+        return reply.toString();
+    }
+
+    public Object addArticle(Long ticketId, Object article) {
+        File file;
+        try {
+            file = File.createTempFile("support", ".json", new File(cranixTmpDir));
+            PrintWriter writer = new PrintWriter(file.getPath(), "UTF-8");
+            writer.print(article);
+            writer.close();
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            return new CrxResponse("ERROR", e.getMessage());
+        }
+        String url = "http://zadmin:9080/api/tickets/regcodes/" + regCode + "/" + ticketId;
+        String[] program = new String[11];
+        StringBuffer reply = new StringBuffer();
+        StringBuffer stderr = new StringBuffer();
+        program[0] = "/usr/bin/curl";
+        program[1] = "--insecure";
+        program[2] = "-s";
+        program[3] = "-X";
+        program[4] = "POST";
+        program[5] = "--header";
+        program[6] = "Content-Type: application/json";
+        program[7] = "--header";
+        program[8] = "Accept: application/json";
+        program[9] = "-d";
+        program[10] = "@" + file.getPath();
+        program[11] = url;
+        CrxSystemCmd.exec(program, reply, stderr, null);
+        return reply.toString();
+    }
 }
