9c26a1b27590fb028fa8d829e33bf4bba19ec8d8
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2026-05-09 21:21:00 +0200

    Adapt positive lists

--- data/updates/010-adapt-database.sh
+++ data/updates/010-adapt-database.sh
@@ -13,6 +13,7 @@ echo "ALTER TABLE Sessions MODIFY validFrom timestamp;" | mysql CRX
 echo "ALTER TABLE Crx2faSessions DROP FOREIGN KEY  FK_Sessions_session_id;" | mysql CRX
 echo "ALTER TABLE Crx2faSessions DROP session_id;" | mysql CRX
 echo "ALTER TABLE Users RENAME COLUMN telefonNumber TO telephoneNumber;" | mysql CRX
+echo "ALTER TABLE PositiveLists DROP subject;" | mysql CRX
 sleep 10
 if [ ${CRANIX_TYPE} = "cephalix" ]; then
 	/usr/bin/systemctl start cephalix-api
