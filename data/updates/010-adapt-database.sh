f8121833aee9820e4bdf9ad183c003d691d074ed
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2026-09-04 22:23:28 +0200

    Adapt role of cephalix. He must be sysadmins not internal

--- data/updates/010-adapt-database.sh
+++ data/updates/010-adapt-database.sh
@@ -6,14 +6,15 @@ else
 	/usr/bin/systemctl stop cranix-api
 fi
 sleep 10
-echo "ALTER TABLE Sessions DROP FOREIGN KEY FK_Sessions_crx2fasession_id" | mysql CRX
-echo "ALTER TABLE Sessions DROP crx2fasession_id;" | mysql CRX
-echo "ALTER TABLE Sessions MODIFY validUntil timestamp;" | mysql CRX
-echo "ALTER TABLE Sessions MODIFY validFrom timestamp;" | mysql CRX
-echo "ALTER TABLE Crx2faSessions DROP FOREIGN KEY  FK_Sessions_session_id;" | mysql CRX
-echo "ALTER TABLE Crx2faSessions DROP session_id;" | mysql CRX
-echo "ALTER TABLE Users RENAME COLUMN telefonNumber TO telephoneNumber;" | mysql CRX
-echo "ALTER TABLE PositiveLists DROP subject;" | mysql CRX
+echo "ALTER TABLE Sessions DROP FOREIGN KEY FK_Sessions_crx2fasession_id" | mariadb CRX
+echo "ALTER TABLE Sessions DROP crx2fasession_id;" | mariadb CRX
+echo "ALTER TABLE Sessions MODIFY validUntil timestamp;" | mariadb CRX
+echo "ALTER TABLE Sessions MODIFY validFrom timestamp;" | mariadb CRX
+echo "ALTER TABLE Crx2faSessions DROP FOREIGN KEY  FK_Sessions_session_id;" | mariadb CRX
+echo "ALTER TABLE Crx2faSessions DROP session_id;" | mariadb CRX
+echo "ALTER TABLE Users RENAME COLUMN telefonNumber TO telephoneNumber;" | mariadb CRX
+echo "ALTER TABLE PositiveLists DROP subject;" | mariadb CRX
+echo "update Users set role='sysadmins' where uid='cephalix';" | mariadb  CRX
 sleep 10
 if [ ${CRANIX_TYPE} = "cephalix" ]; then
 	/usr/bin/systemctl start cephalix-api
