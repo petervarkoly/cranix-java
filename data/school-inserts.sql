65739350ddfb16bc53f7e704cb05b3bc9ca1175c
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2026-09-04 22:27:52 +0200

    Adapt role of cephalix. He must be sysadmins not internal

--- data/school-inserts.sql
+++ data/school-inserts.sql
@@ -103,7 +103,7 @@ INSERT INTO Users SET id=2,uid='tteachers',role='teachers',surName='for teachers
 INSERT INTO Users SET id=3,uid='tstudents',role='students',surName='for students',givenName='Default profile',birthDay=NOW(),fsQuota=0,fsQuotaUsed=0,msQuota=0,msQuotaUsed=0;
 INSERT INTO Users SET id=4,uid='tadministration',role='administration',surName='for administration',givenName='Default profile',birthDay=NOW(),fsQuota=0,fsQuotaUsed=0,msQuota=0,msQuotaUsed=0;
 INSERT INTO Users SET id=5,uid='tworkstations',role='workstations',surName='for workstations',givenName='Default profile',birthDay=NOW(),fsQuota=0,fsQuotaUsed=0,msQuota=0,msQuotaUsed=0;
-INSERT INTO Users SET id=6,uid='cephalix',role='internal',surName='Administrator',givenName='Internal',birthDay=NOW(),fsQuota=0,fsQuotaUsed=0,msQuota=0,msQuotaUsed=0;
+INSERT INTO Users SET id=6,uid='cephalix',role='sysadmins',surName='Administrator',givenName='Internal',birthDay=NOW(),fsQuota=0,fsQuotaUsed=0,msQuota=0,msQuotaUsed=0;
 INSERT INTO Users SET id=7,uid='register',role='internal',surName='Register',givenName='Internal',birthDay=NOW(),fsQuota=0,fsQuotaUsed=0,msQuota=0,msQuotaUsed=0;
 INSERT INTO Users SET id=8,uid='ossreader',role='internal',surName='Reader',givenName='Account',birthDay=NOW(),fsQuota=0,fsQuotaUsed=0,msQuota=0,msQuotaUsed=0;
 CREATE TABLE CrxNextID (
